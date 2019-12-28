cdf05773ae5af38800b48c3899a2188239f2565b
soojung <blessme333@gmail.com>
2019-12-28 13:38:32 +0900

    로그인 요청 후 성공/실패 분기처리

--- app/src/main/java/com/soojung/a20191228_01_api/LoginActivity.kt
+++ app/src/main/java/com/soojung/a20191228_01_api/LoginActivity.kt
@@ -1,8 +1,12 @@
 package com.soojung.a20191228_01_api
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.soojung.a20191228_01_api.utils.ConnectServer
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
+import kotlin.math.log
 
 class LoginActivity : BaseActivity() {
 
@@ -48,7 +52,39 @@ class LoginActivity : BaseActivity() {
 
 //          여기의 코드는 ID / PW 가 모두 정상적이어야만 실행 가능.
 
-            Toast.makeText(mContext, "정상 입력이라 로그인 시도해야함", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(mContext, "정상 입력이라 로그인 시도해야함", Toast.LENGTH_SHORT).show()
+
+            ConnectServer.postRequestLogin(mContext, inputId, inputPw, object : ConnectServer.JsonResponseHandler{
+
+                override fun onResponse(json: JSONObject) {
+
+//                    ID : test_user  / PW : qlalfqjsgh!
+
+                    Log.d("서버응답json", json.toString())
+
+//                    서버에서 돌려주는 code가 몇인지 "Int" 값 확인
+
+                    val code = json.getInt("code")
+
+//                    code : 200 이면 로그인 성공 토스트, code : 200 아니면 로그인 실패 토스트
+
+                    runOnUiThread {
+
+                    if (code == 200) {
+                        Toast.makeText(mContext,"로그인 성공", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        Toast.makeText(mContext,"로그인 실패", Toast.LENGTH_SHORT).show()
+
+                    }
+
+                    }
+                }
+
+
+            } )
+
+
 
         }
 
